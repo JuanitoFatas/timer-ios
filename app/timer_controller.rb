c0f686d3ce6a5e86357b05ec9cf9954408a03c38
Juanito Fatas <katehuang0320@gmail.com>
2015-06-03 14:56:56 +0800

    Create 'Tap to start' label

--- app/timer_controller.rb
+++ app/timer_controller.rb
@@ -1,5 +1,14 @@
 class TimerController < UIViewController
   def viewDidLoad
-    puts "TimerController loaded"
+    margin = 20
+
+    @state = UILabel.new
+    @state.font = UIFont.systemFontOfSize(30)
+    @state.text = 'Tap to start'
+    @state.textAlignment = UITextAlignmentCenter
+    @state.textColor = UIColor.whiteColor
+    @state.backgroundColor = UIColor.clearColor
+    @state.frame = [[margin, 200], [view.frame.size.width - margin * 2, 40]]
+    view.addSubview(@state)
   end
 end
